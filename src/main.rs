3405e05fc988e59215598f06bc8216987d7611d5
lucasmolinari <lucas.molinari2003@gmail.com>
2024-05-18 02:22:06 -0300

    Add REPL

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,41 @@ mod number;
 mod sya;
 mod tokenizer;
 
+use std::io::{self, Write};
+
 use sya::Sya;
 
-fn main() -> Result<(), String> {
-    let mut sya = Sya::new("5 * 3.5")?;
-    match sya.calculate() {
-        Ok(_) => {}
-        Err(e) => println!("Error: {}", e),
+fn main() {
+    let mut sya = Sya::new("").expect("Should construct");
+    println!("q! for exit");
+    loop {
+        let mut input = String::new();
+        print!("> ");
+        io::stdout().flush().expect("Error flushing stdout");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("error: unable to read input");
+
+        if input.trim() == "q!".to_string() {
+            break;
+        }
+
+        match sya.new_input(&input.trim()) {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("Error parsing input: {}", e);
+                continue;
+            }
+        };
+
+        match sya.calculate() {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("Error calculating expression: {}", e);
+                continue;
+            }
+        }
+        println!("RPN: {}", sya.rpn_formatted());
+        println!("Result: {:?}", &sya.out.as_ref().unwrap());
     }
-    println!("RPN: {}", sya.rpn_formatted());
-    println!("Result: {:?}", sya.out.unwrap());
-    Ok(())
 }
